d3b2359042f72759b1d114438356df388520c2ee
Mopcho <valeri_petkov01@abv.bg>
2022-11-02 17:39:10 +0200

    Call to action button animation

--- client/src/components/Hero/Hero.tsx
+++ client/src/components/Hero/Hero.tsx
@@ -9,7 +9,7 @@ export const Hero: FC = () => {
                     <h1 className="text-3xl py-4 leading-10">Tea and Code offers a wide varitety of teas that reduce the bugs you create by up to 80% !</h1>
                     <p className="tracking-widest">And we all know that there are a lot of them </p>
                 </div>
-                <NavLink to="/shop" className="py-4 w-1/4 text-center bg-main text-white rounded-3xl hover:">Shop Now</NavLink>
+                <NavLink to="/shop" className="py-4 w-1/4 text-center bg-main text-white rounded-3xl hover:scale-110">Shop Now</NavLink>
             </div>
             <div className="w-3/6">
                 <img src={require('./hero-image.jpg')} alt="hero" className="mix-blend-multiply"></img>
