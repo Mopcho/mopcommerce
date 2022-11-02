fdb16f314f54c1fe6d8ca3a97dc5a5536ddf7e03
Mopcho <valeri_petkov01@abv.bg>
2022-11-02 18:11:25 +0200

    Responsiveness to Hero Section

--- client/src/components/Hero/Hero.tsx
+++ client/src/components/Hero/Hero.tsx
@@ -3,13 +3,13 @@ import { NavLink } from "react-router-dom";
 
 export const Hero: FC = () => {
     return (
-        <main className="container flex justify-between py-11 text-third">
-            <div className="flex flex-col w-3/6 justify-evenly">
-                <div>
+        <main className="container flex justify-between py-11 text-third flex-col sm:flex-row items-center">
+            <div className="flex flex-col justify-evenly flex-1 w-full sm:w-3/6 items-center sm:items-start gap-5">
+                <div className="text-center sm:text-left">
                     <h1 className="text-3xl py-4 leading-10">Tea and Code offers a wide varitety of teas that reduce the bugs you create by up to 80% !</h1>
                     <p className="tracking-widest">And we all know that there are a lot of them </p>
                 </div>
-                <NavLink to="/shop" className="py-4 w-1/4 text-center bg-main text-white rounded-3xl hover:scale-110">Shop Now</NavLink>
+                <NavLink to="/shop" className="py-4 px-4 w-1/4 text-center bg-main text-white rounded-3xl hover:scale-110">Shop Now</NavLink>
             </div>
             <div className="w-3/6">
                 <img src={require('./hero-image.jpg')} alt="hero" className="mix-blend-multiply"></img>
