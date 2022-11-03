97d4101cf1385d81480f04ab8809de89042b4b0c
Mopcho <valeri_petkov01@abv.bg>
2022-11-03 10:10:58 +0200

    Spread Hero to be full screen

--- client/src/components/Hero/Hero.tsx
+++ client/src/components/Hero/Hero.tsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 
 export const Hero: FC = () => {
     return (
-        <StyledHero className="container flex justify-between py-11 text-third flex-col sm:flex-row items-center relative">
+        <StyledHero className="container flex justify-between py-16 text-third flex-col sm:flex-row items-center relative">
             <div className="flex flex-col justify-evenly flex-1 w-full sm:w-3/6 items-center sm:items-start gap-5 slide-left">
                     <div className="text-center sm:text-left delay-200 ease-in">
                         <h1 className="text-3xl py-4 leading-10">Tea and Code offers a wide varitety of teas that reduce the bugs you create by up to 80% !</h1>
