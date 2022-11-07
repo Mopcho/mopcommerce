90e76560be48c8d8197f79e43264223097310e8d
Mopcho <valeri_petkov01@abv.bg>
2022-11-07 13:58:20 +0200

    Added item hover animation

--- client/src/components/SingleItem/SingleItem.tsx
+++ client/src/components/SingleItem/SingleItem.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 export const SingleItem: FC<{ title: string, price: number, imageUrl: string }> = ({ title, price, imageUrl }) => {
     return (
         <div className="flex flex-col mb-6">
-            <img src={imageUrl} alt="Tea" className="object-cover w-[200px] h-[200px] hover:opacity-70 hover:cursor-pointer scale-95 hover:scale-100"></img>
+            <img src={imageUrl} alt="Tea" className="object-cover w-[200px] h-[200px] hover:opacity-70 hover:cursor-pointer scale-95 hover:scale-100 ease-in duration-100"></img>
             <div className="flex justify-between align-bottom items-center scale-95">
                 <h4 className="text-2xl text-third">{title}</h4>
                 <p className="text-md text-main">{price}$</p>
